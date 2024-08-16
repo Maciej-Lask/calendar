94d6d1245df3c0625db0ee59f88404513ebd198d
Maciej Laskowski <maciek30088@gmail.com>
2024-08-16 15:13:10 +0100

    Fix display bugs

--- src/script.js
+++ src/script.js
@@ -32,6 +32,12 @@ const events = [
     description: 'Omówienie projektu z klientem.',
   },
   {
+    title: 'Spotkanie z klientem',
+    startTime: new Date('2024-08-12T04:00:00'),
+    endTime: new Date('2024-08-12T12:00:00'),
+    description: 'Omówienie projektu z klientem.',
+  },
+  {
     title: 'Lunch',
     startTime: new Date('2024-08-12T13:00:00'),
     endTime: new Date('2024-08-12T13:14:00'),
@@ -46,8 +52,14 @@ const events = [
   {
     title: 'Przygotowanie raportu',
     startTime: new Date('2024-08-14T09:00:00'),
-    endTime: new Date('2024-08-14T12:00:00'),
+    endTime: new Date('2024-08-14T19:00:00'),
     description: 'Przygotowanie raportu kwartalnego.',
+  },
+  {
+    title: 'Spotkanie google meet',
+    startTime: new Date('2024-08-14T08:00:00'),
+    endTime: new Date('2024-08-14T08:30:00'),
+    description: 'Przygotowanie raportu',
   },
 ];
 
@@ -147,6 +159,161 @@ function renderMonthView() {
   calendarView.innerHTML = calendarHTML;
 }
 
+// function renderWeekView() {
+//   const calendarView = document.getElementById('calendarView');
+//   calendarView.innerHTML = '';
+
+//   const daysOfWeek = ['Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'Sb', 'Nd'];
+//   const displayedDate = window.currentDate;
+//   const today = new Date();
+//   const currentDayIndex = (displayedDate.getDay() + 6) % 7;
+
+//   const startDate = new Date(displayedDate);
+//   startDate.setDate(displayedDate.getDate() - currentDayIndex);
+//   startDate.setHours(0, 0, 0, 0);
+//   const endDate = new Date(startDate);
+//   endDate.setDate(startDate.getDate() + 6);
+//   endDate.setHours(23, 59, 59, 999);
+
+//   const weekEvents = events.filter((event) => {
+//     return (
+//       (event.startTime >= startDate && event.startTime <= endDate) ||
+//       (event.endTime >= startDate && event.endTime <= endDate) ||
+//       (event.startTime < startDate && event.endTime > endDate)
+//     );
+//   });
+
+//   const weekContainsToday = today >= startDate && today <= endDate;
+
+//   let calendarHTML = '<div class="header-row">';
+//   calendarHTML += `<div class="calendar-day-header">Godz.</div>`;
+//   for (let i = 0; i < 7; i++) {
+//     const day = new Date(startDate);
+//     day.setDate(startDate.getDate() + i);
+//     const dayOfMonth = day.getDate();
+//     const isToday = today.toDateString() === day.toDateString();
+//     calendarHTML += `<div class="calendar-day-header${
+//       isToday ? ' bg-primary text-white' : ''
+//     }">${daysOfWeek[i]}. ${dayOfMonth}</div>`;
+//   }
+//   calendarHTML += '</div>';
+
+//   calendarHTML += '<div class="calendar-row" style="position: relative;">';
+//   calendarHTML += '<div class="calendar-day-hours">';
+//   for (let hour = 0; hour < 24; hour++) {
+//     calendarHTML += `<div class="calendar-hour">${hour}:00</div>`;
+//   }
+//   calendarHTML += '</div>';
+
+//   for (let i = 0; i < 7; i++) {
+//     const day = new Date(startDate);
+//     day.setDate(startDate.getDate() + i);
+//     const isToday = today.toDateString() === day.toDateString();
+
+//     calendarHTML += `<div class="calendar-day${isToday ? ' current-day' : ''}">
+//                             <div class="calendar-hours" style="position: relative;">`;
+//     for (let hour = 0; hour < 24; hour++) {
+//       calendarHTML += `<div class="calendar-hour"></div>`;
+//     }
+//     calendarHTML += `</div></div>`;
+//   }
+
+//   calendarHTML += '</div>';
+
+//   calendarView.innerHTML = calendarHTML;
+
+//   const dayColumns = calendarView.querySelectorAll(
+//     '.calendar-row .calendar-day'
+//   );
+
+//   // Prepare event groups by day
+//   const eventsByDay = Array.from({ length: 7 }, () => []);
+//   weekEvents.forEach((event) => {
+//     const eventStartDate = new Date(event.startTime);
+//     const startDayIndex = (eventStartDate.getDay() + 6) % 7;
+//     eventsByDay[startDayIndex].push(event);
+//   });
+
+//   // Process each day's events
+//   eventsByDay.forEach((dayEvents, dayIndex) => {
+//     const dayColumn = dayColumns[dayIndex].querySelector('.calendar-hours');
+//     if (!dayColumn) return;
+
+//     // Group overlapping events
+//     const overlappingGroups = [];
+
+//     dayEvents.forEach((event) => {
+//       if (overlappingGroups.length === 0) {
+//         overlappingGroups.push([event]);
+//       } else {
+//         const lastGroup = overlappingGroups[overlappingGroups.length - 1];
+//         const lastEventInGroup = lastGroup[lastGroup.length - 1];
+
+//         if (event.startTime < lastEventInGroup.endTime) {
+//           lastGroup.push(event);
+//         } else {
+//           overlappingGroups.push([event]);
+//         }
+//       }
+//     });
+
+//     console.log(overlappingGroups);
+
+//     overlappingGroups.forEach((group) => {
+//       const groupSize = group.length;
+
+//       group.forEach((event, index) => {
+//         const eventElement = document.createElement('div');
+//         eventElement.className = 'dayViewEvent';
+//         eventElement.style.left = `${(100 / groupSize) * index}%`;
+//         eventElement.style.top = `${
+//           ((event.startTime.getHours() * 60 + event.startTime.getMinutes()) /
+//             1440) *
+//           100
+//         }%`;
+
+//         const durationInMinutes =
+//           event.endTime.getHours() * 60 +
+//           event.endTime.getMinutes() -
+//           (event.startTime.getHours() * 60 + event.startTime.getMinutes());
+
+//         if (durationInMinutes < 60) {
+//           eventElement.style.height = '8%';
+//         } else {
+//           eventElement.style.height = `${(durationInMinutes / 1440) * 100}%`;
+//         }
+
+//         eventElement.innerHTML = `<small>${event.startTime.toLocaleTimeString(
+//           'pl-PL',
+//           { hour: '2-digit', minute: '2-digit' }
+//         )} - ${event.endTime.toLocaleTimeString('pl-PL', {
+//           hour: '2-digit',
+//           minute: '2-digit',
+//         })} ${event.title}</small>`;
+
+//         dayColumn.appendChild(eventElement);
+//       });
+//     });
+//   });
+
+//   if (weekContainsToday) {
+//     const currentHour = today.getHours();
+//     const currentMinute = today.getMinutes();
+//     const currentTimeLineTop =
+//       ((currentHour * 60 + currentMinute) / 1440) * 100;
+
+//     const timeLine = document.createElement('div');
+//     timeLine.className = 'current-time-line';
+//     timeLine.style.top = `${currentTimeLineTop}%`;
+
+//     const currentDayElement = calendarView.querySelector(
+//       '.current-day .calendar-hours'
+//     );
+//     if (currentDayElement) {
+//       currentDayElement.appendChild(timeLine);
+//     }
+//   }
+// }
 function renderWeekView() {
   const calendarView = document.getElementById('calendarView');
   calendarView.innerHTML = '';
@@ -158,7 +325,7 @@ function renderWeekView() {
 
   const startDate = new Date(displayedDate);
   startDate.setDate(displayedDate.getDate() - currentDayIndex);
-  startDate.setHours(0, 0, 0, 0); 
+  startDate.setHours(0, 0, 0, 0);
   const endDate = new Date(startDate);
   endDate.setDate(startDate.getDate() + 6);
   endDate.setHours(23, 59, 59, 999);
@@ -214,6 +381,7 @@ function renderWeekView() {
     '.calendar-row .calendar-day'
   );
 
+  // Prepare event groups by day
   const eventsByDay = Array.from({ length: 7 }, () => []);
   weekEvents.forEach((event) => {
     const eventStartDate = new Date(event.startTime);
@@ -221,50 +389,70 @@ function renderWeekView() {
     eventsByDay[startDayIndex].push(event);
   });
 
+  // Process each day's events
   eventsByDay.forEach((dayEvents, dayIndex) => {
     const dayColumn = dayColumns[dayIndex].querySelector('.calendar-hours');
     if (!dayColumn) return;
 
-    const eventColumnIndexes = [];
+    // Group overlapping events
+    const sortedEvents = dayEvents
+      .slice()
+      .sort((a, b) => a.startTime - b.startTime);
+    const overlappingGroups = [];
 
-    dayEvents.forEach((event, index) => {
-      const eventStartDate = new Date(event.startTime);
-      const eventEndDate = new Date(event.endTime);
-
-      const startHour = eventStartDate.getHours();
-      const startMinute = eventStartDate.getMinutes();
-      const endHour = eventEndDate.getHours();
-      const endMinute = eventEndDate.getMinutes();
-
-      const startInMinutes = startHour * 60 + startMinute;
-      const endInMinutes = endHour * 60 + endMinute;
-      const durationInMinutes = endInMinutes - startInMinutes;
-
-      let columnIndex = 0;
-      while (
-        eventColumnIndexes[columnIndex] &&
-        eventColumnIndexes[columnIndex] > startInMinutes
-      ) {
-        columnIndex++;
+    sortedEvents.forEach((event) => {
+      let added = false;
+      for (const group of overlappingGroups) {
+        if (event.startTime < group[group.length - 1].endTime) {
+          group.push(event);
+          added = true;
+          break;
+        }
       }
-      eventColumnIndexes[columnIndex] = startInMinutes + durationInMinutes;
+      if (!added) {
+        overlappingGroups.push([event]);
+      }
+    });
 
-      const eventElement = document.createElement('div');
-      eventElement.className = 'dayViewEvent';
-      eventElement.style.left = `${columnIndex * 20}%`; 
-      eventElement.style.top = `${(startInMinutes / 1440) * 100}%`;
-      eventElement.style.height = `${(durationInMinutes / 1440) * 100}%`;
-      // eventElement.style.zIndex = 10 + index; 
+    console.log(overlappingGroups);
 
-      eventElement.innerHTML = `<small>${event.startTime.toLocaleTimeString(
-        'pl-PL',
-        { hour: '2-digit', minute: '2-digit' }
-      )} - ${event.endTime.toLocaleTimeString('pl-PL', {
-        hour: '2-digit',
-        minute: '2-digit',
-      })} ${event.title}</small>`;
+    overlappingGroups.forEach((group) => {
+      const groupSize = group.length;
 
-      dayColumn.appendChild(eventElement);
+      group.forEach((event, index) => {
+        const eventElement = document.createElement('div');
+        eventElement.className = 'dayViewEvent';
+        eventElement.style.left = `${(100 / groupSize) * index}%`;
+        eventElement.style.top = `${
+          ((event.startTime.getHours() * 60 + event.startTime.getMinutes()) /
+            1440) *
+          100
+        }%`;
+
+        const durationInMinutes =
+          event.endTime.getHours() * 60 +
+          event.endTime.getMinutes() -
+          (event.startTime.getHours() * 60 + event.startTime.getMinutes());
+
+                if (durationInMinutes < 60) {
+                  eventElement.style.height = '4%';
+                  
+                } else {
+                  eventElement.style.height = `${(durationInMinutes / 1440) * 100}%`;
+                }
+        // eventElement.style.height = `${(durationInMinutes / 1440) * 100}%`;
+        eventElement.style.width = `${100 / groupSize}%`;
+
+        eventElement.innerHTML = `<small>${event.startTime.toLocaleTimeString(
+          'pl-PL',
+          { hour: '2-digit', minute: '2-digit' }
+        )} - ${event.endTime.toLocaleTimeString('pl-PL', {
+          hour: '2-digit',
+          minute: '2-digit',
+        })} ${event.title}</small>`;
+
+        dayColumn.appendChild(eventElement);
+      });
     });
   });
 
@@ -338,39 +526,77 @@ function renderDayView() {
     (event) => event.startTime.toDateString() === displayedDate.toDateString()
   );
 
-  if (dayEvents.length > 0) {
-    dayEvents.forEach((event, index) => {
-      const eventElement = document.createElement('div');
-      eventElement.className = 'dayViewEvent';
-      eventElement.style.left = `${index * 10}%`;
-      eventElement.style.top = `${
-        ((event.startTime.getHours() * 60 + event.startTime.getMinutes()) /
-          1440) *
-        100
-      }%`;
-      eventElement.style.height = `${
-        ((event.endTime.getHours() * 60 +
-          event.endTime.getMinutes() -
-          event.startTime.getHours() * 60 -
-          event.startTime.getMinutes()) /
-          1440) *
-        100
-      }%`;
+  const dayEventsSorted = dayEvents.sort((a, b) => {
+    return a.startTime - b.startTime;
+  });
 
-      // eventElement.style.zIndex = 10 + index;
+  if (dayEventsSorted.length > 0) {
+    const overlappingGroups = [];
 
-      eventElement.innerHTML = `<small>${event.startTime.toLocaleTimeString(
-        'pl-PL',
-        { hour: '2-digit', minute: '2-digit' }
-      )} - ${event.endTime.toLocaleTimeString('pl-PL', {
-        hour: '2-digit',
-        minute: '2-digit',
-      })} ${event.title}</small>`;
+    // Find overlapping events
+    dayEventsSorted.forEach((event, index) => {
+      if (overlappingGroups.length === 0) {
+        overlappingGroups.push([event]);
+      } else {
+        const lastGroup = overlappingGroups[overlappingGroups.length - 1];
+        const lastEventInGroup = lastGroup[lastGroup.length - 1];
 
-      const currentDayColumn = calendarView.querySelector(
-        '.current-day-column .calendar-row .calendar-hours'
-      );
-      currentDayColumn.appendChild(eventElement);
+        if (event.startTime < lastEventInGroup.endTime) {
+          lastGroup.push(event);
+        } else {
+          overlappingGroups.push([event]);
+        }
+      }
+    });
+    console.log(overlappingGroups);
+    overlappingGroups.forEach((group) => {
+      const groupSize = group.length;
+
+      group.forEach((event, index) => {
+        const eventElement = document.createElement('div');
+        eventElement.className = 'dayViewEvent';
+        eventElement.style.top = `${
+          ((event.startTime.getHours() * 60 + event.startTime.getMinutes()) /
+            1440) *
+          100
+        }%`;
+
+        if (
+          event.endTime.getHours() * 60 +
+            event.endTime.getMinutes() -
+            event.startTime.getHours() * 60 -
+            event.startTime.getMinutes() <
+          31
+        ) {
+          eventElement.style.height = '4%';
+        } else {
+          eventElement.style.height = `${
+            ((event.endTime.getHours() * 60 +
+              event.endTime.getMinutes() -
+              event.startTime.getHours() * 60 -
+              event.startTime.getMinutes()) /
+              1440) *
+            100
+          }%`;
+        }
+
+        // Adjust width and position to account for overlapping events
+        eventElement.style.width = `${100 / groupSize}%`;
+        eventElement.style.left = `${(100 / groupSize) * index}%`;
+
+        eventElement.innerHTML = `<small>${event.startTime.toLocaleTimeString(
+          'pl-PL',
+          { hour: '2-digit', minute: '2-digit' }
+        )} - ${event.endTime.toLocaleTimeString('pl-PL', {
+          hour: '2-digit',
+          minute: '2-digit',
+        })} ${event.title}</small>`;
+
+        const currentDayColumn = calendarView.querySelector(
+          '.current-day-column .calendar-row .calendar-hours'
+        );
+        currentDayColumn.appendChild(eventElement);
+      });
     });
   }
 
